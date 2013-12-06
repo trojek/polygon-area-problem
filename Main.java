179fc9348b7f2edf146bd9605881bc5414b2feb8
Tomasz Rojek <tomek@rojek.cc>
2013-12-06 14:04:58 +0100

    add db connection

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws SQLException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException {
+		
+		// Variables needed to connect with database
+		// String database = args[0];
+		String database = "jdbc:mysql://localhost/convex?" + "user=root&password=tomaszek77";
+		Connection conn = DriverManager.getConnection(database);
+		Statement st = conn.createStatement();
+
+		// close db connection
+		conn.close();
+
 
 	}
 
