5573c4a9fc31157d0d2a6ba91e329de517f86798
Tomasz Rojek <tomek@rojek.cc>
2013-12-07 11:01:21 +0100

    add query to db

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.awt.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 
 public class Main {
@@ -12,14 +15,37 @@ public class Main {
 	 */
 	public static void main(String[] args) throws SQLException {
 		
+		int numberOfPolygons = 0;
+		
 		// Variables needed to connect with database
 		// String database = args[0];
 		String database = "jdbc:mysql://localhost/convex?" + "user=root&password=tomaszek77";
 		Connection conn = DriverManager.getConnection(database);
 		Statement st = conn.createStatement();
 
+		// Select number unique of z 
+		ResultSet nop = st.executeQuery("SELECT count(DISTINCT z) FROM Ftable;");
+
+		if (nop.next()){
+			numberOfPolygons = nop.getInt(1);
+		}
+		
+		float[] functionZ = new float[numberOfPolygons];
+		
+		// select unique z
+		ResultSet z = st.executeQuery("SELECT z FROM Ftable group by z");
+		
+		int counter = 0;
+		while (z.next()){
+			functionZ[counter] = z.getFloat(1);
+			counter++;
+		}
+		
+		//SELECT x,y FROM Ftable WHERE CAST(z AS DECIMAL) = CAST(22.23 AS DECIMAL)
 		// close db connection
 		conn.close();
+		
+		
 
 
 	}
