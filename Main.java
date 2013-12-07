090e49e2e096db4d1d985e75d4cf029fdf5c028b
Tomasz Rojek <tomek@rojek.cc>
2013-12-07 14:22:00 +0100

    get points from db and put it into array + add graham algorithm

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
-import java.awt.List;
+import java.awt.Point;
+import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-
 
 public class Main {
 
@@ -16,6 +15,7 @@ public class Main {
 	public static void main(String[] args) throws SQLException {
 		
 		int numberOfPolygons = 0;
+		int numberOfPointsPerMaxPolygon = 0;
 		
 		// Variables needed to connect with database
 		// String database = args[0];
@@ -24,28 +24,60 @@ public class Main {
 		Statement st = conn.createStatement();
 
 		// Select number unique of z 
-		ResultSet nop = st.executeQuery("SELECT count(DISTINCT z) FROM Ftable;");
+		ResultSet nop = st.executeQuery("SELECT count(DISTINCT z) FROM Ftable");
 
 		if (nop.next()){
 			numberOfPolygons = nop.getInt(1);
 		}
 		
+		ResultSet nopfmp = st.executeQuery("SELECT count(z) num FROM Ftable GROUP BY z ORDER BY num DESC LIMIT 1");
+		
+		if (nopfmp.next()){
+			numberOfPointsPerMaxPolygon = nopfmp.getInt(1);
+		}
+		
+				
+		float[][][] SetOfPoints = new float[numberOfPolygons][numberOfPointsPerMaxPolygon][2];
 		float[] functionZ = new float[numberOfPolygons];
 		
 		// select unique z
 		ResultSet z = st.executeQuery("SELECT z FROM Ftable group by z");
-		
+
 		int counter = 0;
 		while (z.next()){
 			functionZ[counter] = z.getFloat(1);
 			counter++;
 		}
 		
-		//SELECT x,y FROM Ftable WHERE CAST(z AS DECIMAL) = CAST(22.23 AS DECIMAL)
+		int i = 0;
+		
+		//Save 
+		for(Float x: functionZ) {
+			ResultSet listOfPoints = st.executeQuery("SELECT x,y FROM Ftable WHERE CAST(z AS DECIMAL) = CAST("+ x +"  AS DECIMAL)");
+			int j = 0;
+			while (listOfPoints.next()){
+				SetOfPoints[i][j][0]=listOfPoints.getFloat(1);
+				SetOfPoints[i][j][1]=listOfPoints.getFloat(2);
+
+				j++;
+			}
+			i++;
+		}
 		// close db connection
 		conn.close();
 		
-		
+		// x coordinates
+		int[] xs = {3, 5, -1, 8, -6, 23, 4};
+
+		// y coordinates
+		int[] ys = {9, 2, -4, 3, 90, 3, -11};
+
+		// find the convex hull
+		List<Point> convexHull = GrahamScan.getConvexHull(xs, ys);
+
+		for(java.awt.Point p : convexHull) {
+		    System.out.println(p);
+		}
 
 
 	}
